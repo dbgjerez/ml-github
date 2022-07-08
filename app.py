81c7d89c680544672490d33da08fa8d20ba15bde
David Borrego <dborrego@redhat.com>
2022-07-08 19:35:40 +0200

    english virtual env python

---
+++ app.py
@@ -0,0 +1,22 @@
+from github import Github
+
+# using an access token
+g = Github("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")
+
+class Repo:
+    name: str
+    ts: int
+    views_total: int
+    views_uniques: int
+    clones_total: int
+    clones_uniques: int
+
+
+for repo in g.get_user().get_repos():
+    name = repo.name
+    clones = repo.get_clones_traffic()
+    views = repo.get_views_traffic()
+    for v in views["views"]: 
+
+        print(repo.name, " ==> ", v.timestamp, v.uniques, v.count)
+    #print(repo.name, " ==> ", clones, " ===> ", views)
